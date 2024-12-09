a928a99212f0a96890b395a19cd20f461708625a
gitesteban0 <gitestebanalayon@gmail.com>
2024-12-09 16:35:38 -0400

    cambios en perfil

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -22,24 +22,24 @@ const last_name = sessionStorage.getItem('last_name');
                             <div class="card-body">
                                 <h4 class="subheader">Configuración general</h4>
                                 <div class="list-group list-group-transparent">
-                                    <a href="./settings.html"
+                                    <a href="#"
                                         class="list-group-item list-group-item-action d-flex align-items-center active">Mi
                                         cuenta</a>
                                     <a href="#"
-                                        class="list-group-item list-group-item-action d-flex align-items-center">My
-                                        Notifications</a>
+                                        class="list-group-item list-group-item-action d-flex align-items-center">Mis
+                                        notificaciones</a>
                                     <a href="#"
-                                        class="list-group-item list-group-item-action d-flex align-items-center">Connected
-                                        Apps</a>
-                                    <a href="./settings-plan.html"
-                                        class="list-group-item list-group-item-action d-flex align-items-center">Plans</a>
+                                        class="list-group-item list-group-item-action d-flex align-items-center">Aplicaciones
+                                        conectadas</a>
                                     <a href="#"
-                                        class="list-group-item list-group-item-action d-flex align-items-center">Billing
-                                        &amp; Invoices</a>
+                                        class="list-group-item list-group-item-action d-flex align-items-center">Planes</a>
+                                    <a href="#"
+                                        class="list-group-item list-group-item-action d-flex align-items-center">Facturación
+                                        &amp; Factura</a>
                                 </div>
-                                <h4 class="subheader mt-4">Experience</h4>
+                                <h4 class="subheader mt-4">Experiencia</h4>
                                 <div class="list-group list-group-transparent">
-                                    <a href="#" class="list-group-item list-group-item-action">Give Feedback</a>
+                                    <a href="#" class="list-group-item list-group-item-action">Dar comentarios</a>
                                 </div>
                             </div>
                         </div>
